76439be0fc9b89caeb7ae9b8d35fcb15798d780e
Christopher Slater <chris@snapper.fish>
2016-11-14 09:15:19 +0000

    Add modified gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -4,33 +4,56 @@ const gulp  = require('gulp');
 const tsc   = require('gulp-typescript');
 const typescript = require('typescript');
 
+const polyfills = [
+  'core-js/es6/promise.js',
+  'node_modules/@types/**/*.ts',
+];
+
 const tsDtsProject = tsc.createProject('tsconfig.json', {
-    declaration: true,
-    noResolve: false,
-    typescript,
-    // noEmitOnError: false,
+  declaration: true,
+  noResolve: false,
+  typescript,
 });
 
 gulp.task('build:dts', function() {
   return gulp.src([
     'src/**/*.ts',
-  ])
+  ].concat(polyfills))
   .pipe(tsDtsProject())
-  .on('error', function (err) {
-    process.exit(1);
-  })
+  // .on('error', function (err) {
+  //   process.exit(1);
+  // })
+  .dts
   .pipe(gulp.dest('build/dts'));
 });
 
-var tsEsProject = tsc.createProject('tsconfig.json', { module : 'es2015', typescript });
+const tsEsProject = tsc.createProject('tsconfig.json', {
+  module : 'es6',
+  typescript,
+});
 
 gulp.task('build:es', function() {
-    return gulp.src([
-        'src/**/*.ts'
-    ])
-    .pipe(tsEsProject())
-    .on('error', function (err) {
-        process.exit(1);
-    })
-    .js.pipe(gulp.dest('build/es/'));
+  return gulp.src([
+    'src/**/*.ts'
+  ].concat(polyfills))
+  .pipe(tsEsProject())
+  // .on('error', function (err) {
+  //     process.exit(1);
+  // })
+  .js.pipe(gulp.dest('build/es/'));
 });
+
+var tsJsProject = tsc.createProject('tsconfig.json', typescript);
+
+gulp.task('build:js', function() {
+  return gulp.src([
+    'src/**/*.ts',
+  ].concat(polyfills))
+  .pipe(tsJsProject())
+  // .on('error', function (err) {
+  //     process.exit(1);
+  // })
+  .js.pipe(gulp.dest('build/js/'));
+});
+
+gulp.task('default', ['build:js', 'build:es', 'build:dts']);
